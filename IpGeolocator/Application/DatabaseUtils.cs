c888f0661d1bc263bd161b25f7dd8aa3eb61dffc
Aristarkh Zagorodnikov <xm@drive.net>
2020-04-15 15:17:36 +0300

    Remove unused code

--- IpGeolocator/Application/DatabaseUtils.cs
+++ IpGeolocator/Application/DatabaseUtils.cs
@@ -5,54 +5,10 @@ using System.IO;
 using System.IO.Compression;
 using System.Text;
 
-using static System.FormattableString;
-
 namespace IpGeolocator.Application
 {
     internal static class DatabaseUtils
     {
-        public static I2LDatabase Read(Stream stream)
-        {
-            using var decompressed = new DeflateStream(stream, CompressionMode.Decompress, true);
-            using var buffered = new BufferedStream(decompressed, 65536);
-            using var reader = new BinaryReader(buffered, Encoding.ASCII, true);
-            var version = reader.ReadInt32();
-            if (version != 1)
-            {
-                throw new InvalidDataException(Invariant($"Invalid version {version}."));
-            }
-
-            var atomsCount = reader.ReadInt32();
-            var locationsCount = reader.ReadInt32();
-            var intervalsCount = reader.ReadInt32();
-
-            var atoms = new string[atomsCount];
-            for (var i = 0; i < atomsCount; i++)
-            {
-                atoms[i] = reader.ReadString();
-            }
-
-            var locations = new I2LLocation[locationsCount];
-            for (var i = 0; i < locationsCount; i++)
-            {
-                var countryIndex = reader.ReadInt32();
-                var regionIndex = reader.ReadInt32();
-                var cityIndex = reader.ReadInt32();
-                locations[i] = new I2LLocation(countryIndex, regionIndex, cityIndex);
-            }
-
-            var intervals = new I2LInterval4[intervalsCount];
-            for (var i = 0; i < intervalsCount; i++)
-            {
-                var fromAddress = reader.ReadUInt32();
-                var toAddress = reader.ReadUInt32();
-                var index = reader.ReadInt32();
-                intervals[i] = new I2LInterval4(fromAddress, toAddress, index);
-            }
-
-            return new I2LDatabase(intervals, locations, atoms);
-        }
-
         public static void ConvertFromCsv(Stream input, Stream output)
         {
             var nfi = NumberFormatInfo.InvariantInfo;
