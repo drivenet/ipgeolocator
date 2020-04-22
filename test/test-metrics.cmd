bc12c2155dc7f0e3f0afd8fa31aed50bf77b3036
Aristarkh Zagorodnikov <xm@drive.net>
2020-04-22 16:31:38 +0300

    Cleanup

--- test/test-metrics.cmd
+++ test/test-metrics.cmd
@@ -1,1 +1,3 @@
-@curl http://localhost:5002/v0.1/metrics/location_successes -v
+curl http://localhost:5002/v0.1/metrics/location_successes
+curl http://localhost:5002/v0.1/metrics/location_failures
+curl http://localhost:5002/v0.1/metrics/location_elapsed
