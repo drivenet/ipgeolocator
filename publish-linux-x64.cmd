ec22aa3b1bdac2db1c1e6b1eabe9ceefaf6f49d4
Aristarkh Zagorodnikov <xm@drive.net>
2021-06-24 19:36:35 +0300

    Refresh build scripts

--- publish-linux-x64.cmd
+++ publish-linux-x64.cmd
@@ -1,7 +1,7 @@
 @echo off
 rmdir /s /q packages\linux-x64\ipgeolocator
 mkdir packages\linux-x64\ipgeolocator
-dotnet publish IpGeolocator --output packages\linux-x64\ipgeolocator -c Integration -r linux-x64 --self-contained false
+dotnet publish IpGeolocator --force --output packages\linux-x64\ipgeolocator -c Integration -r linux-x64 --self-contained false
 move packages\linux-x64\ipgeolocator\Microsoft.Extensions.Hosting.Systemd.dll "%TEMP%"
 del packages\linux-x64\ipgeolocator\web.config packages\linux-x64\ipgeolocator\*.deps.json packages\linux-x64\ipgeolocator\Microsoft.*.dll
 move "%TEMP%\Microsoft.Extensions.Hosting.Systemd.dll" packages\linux-x64\ipgeolocator
